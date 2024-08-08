9f13cae828181c20563f1f61ca4f02724a24614f
wrancee <164195907+wrancee@users.noreply.github.com>
2024-08-08 19:39:48 +0800

    Update bag.js

--- bag.js
+++ bag.js
@@ -333,7 +333,7 @@ async function storePackId(packId = '', prizeId = '') {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
         const responseData = await response.json();
-        console.log("data is here", responseData.data);
+        console.log("data is here", responseData.data[0].packId);
         return responseData.data[0].packId; 
     } catch (error) {
         console.error('Error fetching user prize account:', error);
